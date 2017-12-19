6217b5f30aa9e93b89b83b10be4080f9a923b592
duzeyu <vae.dzy@gmail.com>
2017-12-19 20:58:18 +0800

    TestUser

--- src/com/test/UserTest.java
+++ src/com/test/UserTest.java
@@ -1,6 +1,21 @@
 package com.test;
+import com.account.bean.User;
+import com.account.login.service.UserService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring/applicationContext.xml"})
 public class UserTest{
+    @Autowired
+    UserService userService;
+    @Test
+    public void TestUser(){
+        User user = new User("13241997744");
+        user =  userService.findUser(user);
+        System.out.println(user.getId());
+    }
 }
