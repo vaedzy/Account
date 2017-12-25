047a871020e564a402b44429571cfb678b1ad81f
377342263 <377342263@qq.com>
2017-12-25 22:26:02 +0800

    app

---
+++ src/com/account/bean/App.java
@@ -0,0 +1,16 @@
+package com.account.bean;
+
+import java.io.Serializable;
+
+public class App implements Serializable{
+
+    private long aAppname;
+
+    public long getaAppname() {
+        return aAppname;
+    }
+
+    public void setaAppname(long aAppname) {
+        this.aAppname = aAppname;
+    }
+}
