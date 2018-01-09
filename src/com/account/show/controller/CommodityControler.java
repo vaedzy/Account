0275d2cfd73e852897e4c638d733862c7bf316d7
377342263 <377342263@qq.com>
2018-01-09 23:52:58 +0800

    上传bug修改

--- src/com/account/show/controller/CommodityControler.java
+++ src/com/account/show/controller/CommodityControler.java
@@ -104,12 +104,10 @@ public class CommodityControler {
                     if (i==1){
                         goodsInfo.setGphotourl1(path);
                         i++;
-                    }
-                    if (i==2){
+                    }else if (i==2){
                         goodsInfo.setGphotourl2(path);
                         i++;
-                    }
-                    if (i==3){
+                    }else if (i==3){
                         goodsInfo.setGphotourl3(path);
                         i++;
                     }else if (i==4){
