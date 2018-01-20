5cd6f868c476179aef5f436eaf4bc63fb4415c8e
duzeyu <vae.dzy@gmail.com>
2018-01-20 23:37:17 +0800

    修复了service报错

--- src/com/account/realname/service/impl/RealNameServiceImpl.java
+++ src/com/account/realname/service/impl/RealNameServiceImpl.java
@@ -1,6 +1,8 @@
 package com.account.realname.service.impl;
 
 import com.account.realname.service.RealNameService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class RealNameServiceImpl implements RealNameService{
 }
